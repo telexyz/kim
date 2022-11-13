f5d58fcce041c80da50000f55939d3f3f5e7e718
Tien Dung <dungtn@gmail.com>
2022-11-13 20:36:57 +0700

    refine

--- run.sh
+++ run.sh
@@ -8,12 +8,13 @@
 # https://github.com/dlsyscourse/public_notebooks/blob/main/convolution_implementation.ipynb
 # https://github.com/dlsyscourse/public_notebooks/blob/main/rnn_implementation.ipynb
 
-# python3 -m pytest tests/test_cifar_ptb_data.py
 # python3 -m pytest tests/test_conv.py
 # python3 -m pytest tests/test_sequence_models.py
 
 # DONE
 # python3 -m pytest tests/test_nd_backend.py
+# python3 -m pytest tests/test_cifar_ptb_data.py
+
 python3 -m pytest \
 	tests/test_autograd.py \
 	tests/test_init.py \

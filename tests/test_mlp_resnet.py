00dfd283b2f0466c79241d64c3e9d7638d5df7cf
tiendung <dungtn@gmail.com>
2022-11-16 09:16:07 +0700

    correct test_mlp_train_epoch_1 test

--- tests/test_mlp_resnet.py
+++ tests/test_mlp_resnet.py
@@ -119,11 +119,11 @@ def test_mlp_resnet_forward_2():
         atol=1e-5)
 
 def test_mlp_train_epoch_1():
-    np.testing.assert_allclose(train_epoch_1(5, 256, kicm.optim.Adam, lr=0.01, weight_decay=0.1),
+    np.testing.assert_allclose(train_epoch_1(5, 250, kim.optim.Adam, lr=0.01, weight_decay=0.1),
         np.array([0.675267, 1.84043]), rtol=0.0001, atol=0.0001)
 
 def test_mlp_train_speed():
-    train_epoch_1(16, 256, kim.optim.Adam, lr=0.01, weight_decay=0.1)
+    train_epoch_1(16, 250, kim.optim.Adam, lr=0.01, weight_decay=0.1)
 
 def test_mlp_eval_epoch_1():
     np.testing.assert_allclose(eval_epoch_1(10, 150),

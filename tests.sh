3bce93ffa31d63f3bcf2a887f4544e034f5bedf5
tiendung <dungtn@gmail.com>
2022-11-26 13:02:08 +0700

    why test_optim.py fail for nd backend?

--- tests.sh
+++ tests.sh
@@ -3,11 +3,13 @@ echo !!! backend: $KIM_BACKEND, device: $KIM_DEVICE
 python3 -m pytest \
 	tests/test_ndarray.py \
 	tests/test_nd_backend.py \
-	tests/test_conv.py \
 	tests/test_autograd.py \
 	tests/test_init.py \
+	# tests/test_conv.py \
+
+python3 -m pytest \
 	tests/test_nn.py \
 	tests/test_ops.py \
-	tests/test_optim.py
+	tests/test_optim.py \
 
 echo !!! backend: $KIM_BACKEND, device: $KIM_DEVICE

bd515d8a783703046721e5c43b1c755a43ef5c17
Tien Dung <dungtn@gmail.com>
2022-12-10 11:25:02 +0700

    kim works well with cuda 11.7

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+sudo apt install python3-pip build-essential cmake
 pip3 install --upgrade --no-deps git+https://github.com/dlsys10714/mugrade.git
 pip3 install pytest numpy numdifftools pybind11 requests
 make
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117

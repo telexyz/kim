5a4da2ff86ecf97a3e18aa37853f7ef42c857a20
Tien Dung <dungtn@gmail.com>
2022-12-26 13:15:55 +0700

    refine

--- tests/test_project.py
+++ tests/test_project.py
@@ -22,35 +22,30 @@ def test_max_pooling(N,H,W,C):
     # res_torch = torch.nn.MaxPool2d((1, 2))(X_)
     # res_ndl = ndl.ops.max_pooling_1x2(X)
 
-    np.testing.assert_allclose(res_torch.detach().numpy(
-    ), res_ndl.numpy(), rtol=1e-04, atol=1e-04)
+    np.testing.assert_allclose(res_torch.detach().numpy(), res_ndl.numpy(), rtol=1e-04, atol=1e-04)
 
     # backward
     res_torch.sum().backward()
     res_ndl.sum().backward()
-    res_torch_grad = X_.grad.numpy()
-    res_ndl_grad = X.grad.numpy()
-    np.testing.assert_allclose(res_torch_grad, res_ndl_grad, rtol=1e-04, atol=1e-04)
+    np.testing.assert_allclose(X_.grad.numpy(), X.grad.numpy(), rtol=1e-04, atol=1e-04)
 
 
 @pytest.mark.parametrize("N", [1, 2])
 @pytest.mark.parametrize("H", [4, 6, 16])
 @pytest.mark.parametrize("W", [1, 3, 100])
-@pytest.mark.parametrize("C_in", [1, 3, 5])
-@pytest.mark.parametrize("C_out", [1, 16, 32])
+@pytest.mark.parametrize("C_in", [1, 5])
+@pytest.mark.parametrize("C_out", [1, 32])
 @pytest.mark.parametrize("kh", [5, 1, 3])
 @pytest.mark.parametrize("kw", [1, 4, 9])
 def test_conv_kernel_hw(N, H, W, C_in, C_out, kh, kw):
-    seed = 1
-    X = np.random.default_rng(seed=seed).normal(size=(N, C_in, H, W))
-
-    X_ = torch.Tensor(X)
+    X = ndl.init.randn(N, C_in, H, W, requires_grad=True)
+    X_ = torch.Tensor(X.cached_data.numpy())
     X_.requires_grad = True
-    X = ndl.Tensor(X)
-    # transpose X as well because in NN.Conv, use pytorch convention.
 
     imp_ndl = ndl.nn.Conv(C_in, C_out, (kh, kw))
     imp_torch = torch.nn.Conv2d(C_in, C_out, (kh, kw), padding=(kh//2, kw//2))
+
+    # Ensure ndl and torch have same init params (transpose(3, 2, 0, 1) = KKIO -> OIKK)
     imp_torch.weight.data = torch.tensor(imp_ndl.weight.cached_data.numpy().transpose(3, 2, 0, 1))
     imp_torch.bias.data = torch.tensor(imp_ndl.bias.cached_data.numpy())
 
@@ -59,11 +54,9 @@ def test_conv_kernel_hw(N, H, W, C_in, C_out, kh, kw):
     res_ndl = imp_ndl(X)
 
     np.testing.assert_allclose(res_torch.shape, res_ndl.shape)
-    np.testing.assert_allclose(res_torch.detach().numpy(), res_ndl.detach().numpy(), rtol=1e-04, atol=1e-04)
+    np.testing.assert_allclose(res_torch.detach().numpy(), res_ndl.numpy(), rtol=1e-04, atol=1e-04)
 
     # backward
     res_torch.sum().backward()
     res_ndl.sum().backward()
-    res_torch_grad = X_.grad.numpy()
-    res_ndl_grad = X.grad.numpy()
-    np.testing.assert_allclose(res_torch_grad, res_ndl_grad, rtol=1e-04, atol=1e-04)
+    np.testing.assert_allclose(X_.grad.numpy(),  X.grad.numpy(), rtol=1e-04, atol=1e-04)
